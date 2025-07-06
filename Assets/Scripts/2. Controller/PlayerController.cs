a0b055c646bf7743041b4069acbe4c66c833f2ed
znlsnel <dnel0605@gmail.com>
2025-07-06 17:55:36 +0900

    [Feat] PlayerController
    - 플레이어 이동 로직 구현
    - 마우스 클릭 & 드래그를 통해 이동할 수 있도록 설계

--- Assets/Scripts/2. Controller/PlayerController.cs
+++ Assets/Scripts/2. Controller/PlayerController.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 5f;
+
+
+    private InputAction Click;
+    private InputAction Pointer;
+
+    private bool isMove = false;
+
+    private Vector3 startPos = Vector3.zero;
+    private Vector3 mousePos = Vector3.zero;
+
+    private void Start()
     {
-        
+        Click = Managers.Input.Click;
+        Pointer = Managers.Input.Pointer;
+
+        Click.started += OnClick;
+        Click.canceled += OnClick;
+
+        Pointer.performed += OnPointer;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (isMove)
+            OnMove();
+    }
+
+    private void OnClick(InputAction.CallbackContext obj)
+    {
+        isMove = obj.phase == InputActionPhase.Started;
+        if (isMove)
+        {
+            startPos = Input.mousePosition;
+            mousePos = startPos; 
+        }
+
+    }
+
+
+    private void OnPointer(InputAction.CallbackContext obj)
+    {
+        if (!isMove)
+            return;
+
+        mousePos = obj.ReadValue<Vector2>();
+    }
+
+
+    private void OnMove()
+    {
+        if (Vector3.Distance(mousePos, startPos) < 0.1f) 
+            return;
+
+        Vector3 dir = (mousePos - startPos).normalized;
+        transform.position += dir * moveSpeed * Time.deltaTime;
     }
 }
